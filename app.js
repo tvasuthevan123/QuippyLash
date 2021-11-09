20b6356d5720e38cb2ca564afe75c724cfda9f22
Oliver <oliver@curiosity.clients.soton.ac.uk>
2021-11-09 16:13:37 +0000

    Initial application

---
+++ app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+//Set up express
+const express = require('express');
+const app = express();
+
+//Setup socket.io
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+//Setup static page handling
+app.set('view engine', 'ejs');
+app.use('/static', express.static('public'));
+
+//Handle client interface on /
+app.get('/', (req, res) => {
+  res.render('client', {version: process.version});
+});
+//Handle display interface on /display
+app.get('/display', (req, res) => {
+  res.render('display', {version: process.version});
+});
+
+//Start the server
+function startServer() {
+    const PORT = process.env.PORT || 8080;
+    server.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+}
+
+//Chat message
+function handleChat(message) {
+    console.log('Handling chat: ' + message); 
+    io.emit('chat',message);
+}
+
+//Handle new connection
+io.on('connection', socket => { 
+  console.log('New connection');
+
+  //Handle on chat message received
+  socket.on('chat', message => {
+    handleChat(message);
+  });
+
+  //Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('Dropped connection');
+  });
+});
+
+//Start server
+if (module === require.main) {
+  startServer();
+}
+
+module.exports = server;
---
+++ public/game.js
@@ -0,0 +1,55 @@
+var socket = null;
+
+//Prepare game
+var app = new Vue({
+    el: '#game',
+    data: {
+        connected: false,
+        messages: [],
+        chatmessage: '',
+    },
+    mounted: function() {
+        connect(); 
+    },
+    methods: {
+        handleChat(message) {
+            if(this.messages.length + 1 > 10) {
+                this.messages.pop();
+            }
+            this.messages.unshift(message);
+        },
+        chat() {
+            socket.emit('chat',this.chatmessage);
+            this.chatmessage = '';
+        },
+    }
+});
+
+function connect() {
+    //Prepare web socket
+    socket = io();
+
+    //Connect
+    socket.on('connect', function() {
+        //Set connected state to true
+        app.connected = true;
+    });
+
+    //Handle connection error
+    socket.on('connect_error', function(message) {
+        alert('Unable to connect: ' + message);
+    });
+
+    //Handle disconnection
+    socket.on('disconnect', function() {
+        alert('Disconnected');
+        app.connected = false;
+    });
+
+    //Handle incoming chat message
+    socket.on('chat', function(message) {
+        app.handleChat(message);
+    });
+
+
+}
